c3257559793c56663f2b37be1ae309715d73e5fd
Gary Ditsch <garyditsch@gmail.com>
2016-12-08 16:12:02 -0500

    update api settings

--- conf/settings/base.py
+++ conf/settings/base.py
@@ -87,7 +87,6 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config()
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2500,
 }
 
