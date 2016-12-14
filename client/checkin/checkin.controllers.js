7d2c963158bbe346da8f842a155d8950092b881b
Gary Ditsch <garyditsch@gmail.com>
2016-12-14 14:25:34 -0500

    adding race name helper function to checkin

--- client/checkin/checkin.controllers.js
+++ client/checkin/checkin.controllers.js
@@ -12,5 +12,13 @@ function CheckinController(checkinsSaveService) {
             ctrl.editedCheckin = {};
         });
     };
+
+    ctrl.getRaceName = function getRaceName(raceid) {
+        for (let i = 0; i < ctrl.races.length; i += 1) {
+            if (raceid === ctrl.races[i].id) {
+                return ctrl.races[i].name;
+            }
+        }
+    };
 }
 export default CheckinController;
